9345821f1deac8d20738ea77ca38813d2cc85717
Juan Veras <juan@vvssistemas.com.br>
2025-09-10 21:45:52 -0300

    Controle de fluxo --> Verificando fome

---
+++ asimov.academy/controle_de_fluxo/check_hunger.py
@@ -0,0 +1,14 @@
+print('----- INÍCIO -----\n')
+
+tenhoFome = input('Estou com fome? (Digite s para sim)');
+tenhoComida = input('Tenho comida em casa? (Digite s para sim)');
+
+if tenhoFome == 's':
+    if tenhoComida != 's':
+        print('Ir ao mercado')
+        print('Voltar para casa')
+    print('Preparar uma refeição')
+    print('Comer a comida')
+
+print('\n----- FIM -----')
+    
